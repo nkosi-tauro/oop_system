53332c16a2133aec6058ab5343dc280f24811c5e
nkosi-tauro <nkosilati23@yahoo.com>
2023-04-06 17:08:20 +0200

    Added opencv

--- Assignment 2 - System Implementation/driverless_car.py
+++ Assignment 2 - System Implementation/driverless_car.py
@@ -3,8 +3,11 @@ This python program is based on the design of the driverless car system.
 I used my original design classes then added on 
 what I thought could be additional scenarios for the prgram to handle.
 """
+# pylint: disable=maybe-no-member
 import random
 from abc import ABC, abstractmethod
+import numpy as np
+import cv2
 
 import logging
 
@@ -45,7 +48,7 @@ class Control:
     def execute_decision(self, decision):
         """Executes the decision from the decision classes."""
         self.sensor_data[decision] = decision
-        print(f"Executing decision: {decision}")
+        print(f"\nExecuting decision: {decision}")
 
 
 # Exception classes
@@ -205,6 +208,94 @@ class TrafficSignalRecognitionDecision(Decision):
             logging.error(err)
             return "Unable to determine traffic light state."
 
+# This version of the Traffic Signal Recognition Decision class is a bit more complex than the others.
+# It utilizies the OpenCV library to detect the color of the traffic light.
+class TrafficSignalRecognitionSensorCV(Sensor):
+    """
+    The Traffic Signal Recognition Sensor class will process data
+    and pass it on to the corresponding decision class.
+    """
+
+    def process_data(self):
+        """
+        Use trafficstop.jpg to test for red
+        Use trafficgo.jpg to test for green
+        Use trafficslow.jpg to test for yellow
+        One may need to adjust the HSV values to improve accuracy for other images
+        """
+        # Important: Please note depending on whether you are using a Mac\Linux or Windows machine,
+        # you may need to change the path to the image file (Relative or Absolute Path).
+        image = cv2.imread("Assignment 2 - System Implementation\\trafficgo.jpg")
+        logging.info("Processing traffic recognition sensor data...")
+        try:
+            # Convert camera data to HSV format for better color detection
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            # Define color ranges for red, green, and yellow signals
+            red_lower = np.array([0, 70, 50])
+            red_upper = np.array([5, 255, 255])
+            red_lower2 = np.array([175, 70, 50])
+            red_upper2 = np.array([180, 255, 255])
+            green_lower = np.array([35, 50, 50])
+            green_upper = np.array([80, 255, 255])
+            yellow_lower = np.array([15, 100, 100])
+            yellow_upper = np.array([40, 255, 255])
+            red_mask = cv2.inRange(hsv, red_lower, red_upper)
+            red_mask2 = cv2.inRange(hsv, red_lower2, red_upper2)
+            red_mask = cv2.bitwise_or(red_mask, red_mask2)
+            green_mask = cv2.inRange(hsv, green_lower, green_upper)
+            yellow_mask = cv2.inRange(hsv, yellow_lower, yellow_upper)
+            # Combine masks to get final signal detection
+            mask = cv2.bitwise_or(cv2.bitwise_or(
+                red_mask, green_mask), yellow_mask)
+
+            # Find contours in the image and extract the largest one
+            contours, _ = cv2.findContours(
+                mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            if len(contours) > 0:
+                color = ""
+                largest_contour = max(contours, key=cv2.contourArea)
+                if cv2.contourArea(largest_contour) > 1000:
+                    x, y, w, h = cv2.boundingRect(largest_contour)
+                    # Determine the color of the bounding box based on the detected color
+                    if cv2.countNonZero(red_mask[y:y+h, x:x+w]) > 0:
+                        color = 'red'
+                    elif cv2.countNonZero(green_mask[y:y+h, x:x+w]) > 0:
+                        color = 'green'
+                    elif cv2.countNonZero(yellow_mask[y:y+h, x:x+w]) > 0:
+                        color = 'yellow'
+
+                return {"traffic_signal": color}
+
+            else:
+                logging.warning("No traffic signal detected.")
+                return {"traffic_signal": "none"}
+        except InvalidSensorDataError as err:
+            logging.error("Error processing data from traffic sensor: %s", err)
+            return {}
+
+class TrafficSignalRecognitionDecisionCV(Decision):
+    """ 
+    The traffic signal recognition decision class will process 
+    the data from the traffic signal recognition sensor.
+    """
+
+    def make_decision(self, data):
+        try:
+            if not data:
+                raise InvalidDecisionError(
+                    "Invalid data for traffic signal recognition decision.")
+            if data["traffic_signal"] == "red":
+                return "Stop"
+            elif data["traffic_signal"] == "yellow":
+                return "Slow Down"
+            elif data["traffic_signal"] == "green":
+                return "Go"
+            else:
+                raise InvalidDecisionError(
+                    "Unable to determine traffic light state.")
+        except InvalidDecisionError as err:
+            logging.error(err)
+            return "Unable to determine traffic light state." 
 
 # Main program
 def start_driverless_car(choice):
@@ -213,19 +304,34 @@ def start_driverless_car(choice):
         lane_sensor = LaneDetectionSensor()
         obstacle_sensor = ObstacleAvoidanceSensor()
         traffic_sensor = TrafficSignalRecognitionSensor()
+        traffic_sensor_cv = TrafficSignalRecognitionSensorCV()
 
         lane_decision = LaneDetectionDecision()
         obstacle_decision = ObstacleAvoidanceDecision()
         traffic_decision = TrafficSignalRecognitionDecision()
+        traffic_decision_cv = TrafficSignalRecognitionDecisionCV()
 
         control = Control()
 
-        if choice in range(1, 4):
-            sensors = [lane_sensor, obstacle_sensor, traffic_sensor]
-            decisions = [lane_decision, obstacle_decision, traffic_decision]
 
-            sensor_data = sensors[choice - 1].process_data()
-            decision = decisions[choice - 1].make_decision(sensor_data)
+
+        if choice in range(1, 5):
+            sensor_classes = [lane_sensor, obstacle_sensor, traffic_sensor, traffic_sensor_cv]
+            decision_classes = [lane_decision, obstacle_decision, 
+                                traffic_decision, traffic_decision_cv]
+
+            sensor_data = sensor_classes[choice - 1].process_data()
+            decision = decision_classes[choice - 1].make_decision(sensor_data)
+
+
+            # Logging Output
+            sensor = sensor_classes[choice - 1] # get which sensor class was active
+            dec = decision_classes[choice - 1] # get which decision class was active
+            logging.info("Sensor: %s, Decision: %s",
+                        sensor.__class__.__name__, dec.__class__.__name__)
+            logging.info("Raw data: %s", sensor_data)
+            logging.info("Analysis result: %s", decision)
+
             control.execute_decision(decision)
         else:
             raise ValueError("Invalid choice.")
@@ -240,7 +346,8 @@ def get_user_choice():
     print("1. Lane Detection Sensor")
     print("2. Obstacle Avoidance Sensor")
     print("3. Traffic Signal Recognition Sensor")
-    print("4. Exit\n")
+    print("4. Traffic Signal Recognition Sensor using OpenCV")
+    print("5. Exit\n")
     try:
         return int(input("Enter your choice: "))
     except ValueError:
@@ -253,6 +360,6 @@ if __name__ == "__main__":
 
     while True:
         user_choice = get_user_choice()
-        if user_choice == 4:
+        if user_choice == 5:
             break
         start_driverless_car(user_choice)
